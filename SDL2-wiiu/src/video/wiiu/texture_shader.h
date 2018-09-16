79ef580a605f9d35cd4320a2d1dc268b38ce6d7b
rw-r-r-0644 <r.r.qwertyuiop.r.r@gmail.com>
2018-09-16 14:55:37 +0200

    Change shader include guard to match SDL

--- SDL2-wiiu/src/video/wiiu/texture_shader.h
+++ SDL2-wiiu/src/video/wiiu/texture_shader.h
@@ -1,4 +1,7 @@
-#pragma once
+#ifndef _WIIU_texture_shader_h
+#define _WIIU_texture_shader_h
 
 extern unsigned char texture_shader_gsh[];
 extern unsigned int texture_shader_gsh_len;
+
+#endif /* _WIIU_texture_shader_h */
